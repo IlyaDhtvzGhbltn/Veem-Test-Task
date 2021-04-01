bb9b3f0aa58f1c810fad881c7d23e850c80fd5cd
Ilya Ryazanov <ilya.ryazanov@quipu.de>
2021-04-01 10:58:31 +0300

    UnhandledExceptions delegate added

--- ProcessKiller/Program.cs
+++ ProcessKiller/Program.cs
@@ -11,11 +11,24 @@ namespace ProcessKiller
 {
     class Program
     {
+        static IOutput output = new ConsoleOut();
+
         static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionCatcher;
             var application = new CommandLineApplication();
-            application.Command("kill", (a) => new KillByTimer(new ConsoleOut()).Invoke(a));
+            application.Command("kill", (a) => new KillByTimer(output).Invoke(a));
             application.Execute(args);
+
+            Console.ReadKey();
         }
+
+        static void UnhandledExceptionCatcher(object sender, UnhandledExceptionEventArgs e)
+        {
+            output.Error(e.ExceptionObject.ToString());
+            var ex = e.ExceptionObject as Exception;
+            output.Error(ex.StackTrace);
+        }
+
     }
 }
